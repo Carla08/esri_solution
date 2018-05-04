f67e9a36d509fd13e4a3cde06898e290f9a60d4e
Carla Prieto <carla.prieto@spectrumeffect.com>
2018-05-04 07:28:15 -0500

    Added more docstring to solution.py

--- solution_esri/solution.py
+++ solution_esri/solution.py
@@ -1,7 +1,7 @@
 import argparse
 
 
-def next_number_generator(n: int):
+def next_number_generator(n: int) -> int:
     while n > 1:
         n = n//2 if n % 2 == 0 else 3*n + 1
         yield n
@@ -14,7 +14,7 @@ def calculate_seq_len(n: int) -> int:
     return len(list(next_number_generator(n)))
 
 
-def get_the_nth_number(n: int, index: int):
+def get_the_nth_number(n: int, index: int) -> int:
     if n == 0:
         raise SequenceError
 
@@ -24,11 +24,19 @@ def get_the_nth_number(n: int, index: int):
 
 
 class SequenceError(ValueError):
+    """
+    Custom Error to indicate the sequence does not end un 1.
+    """
     def __init__(self):
         super().__init__("Sequence does not end in 1")
 
 
 def parse_arguments():
+    """
+    Parse the command line arguments through argparse.
+    Print the help on the command line if cant be parsed.
+    :return:
+    """
     parser = argparse.ArgumentParser(description='To run the solution')
     parser.add_argument('n', type=int, help='number to start the sequence')
     parser.add_argument('--len', help='get the length of the sequence', action='store_true')
@@ -41,6 +49,11 @@ def parse_arguments():
 
 
 def dispatch(args):
+    """
+    Call the selected function in the command line argumments.
+    :param args: (namespace) parsed arguments.
+    :return: (int or None)
+    """
     # if --len argument was entered
     if args.len:
         return calculate_seq_len(args.n)
